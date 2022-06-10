20b3b2b7c6f11783dd487a00aa45cb4e7fb2c0f2
MercenaryHapy6 <corazoneduardo@att.net>
2022-06-09 19:30:45 -0500

    udpated div in return

--- control.js
+++ control.js
@@ -43,7 +43,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className='container'>
+      <div className='background'>
         <div className="player"></div>
       </div>
 
